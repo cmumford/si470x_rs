c116718b9f15cb9755c7761312a8c79a66c74e20
Chris Mumford <cmumford@cmumford.com>
2026-01-16 18:36:30 -0500

    Move register definitions into registers.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
 mod driver;
+mod registers;
 
-pub use driver::{ChipInfo, Si470x, Si470xError, reset_radio_for_i2c};
+pub use driver::{Si470x, Si470xError, reset_radio_for_i2c};
+pub use registers::ChipInfo;
